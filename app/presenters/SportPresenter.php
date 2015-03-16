bd3e7321a080cfbdb765294ed58ce5d7b77a92c6
Ivo Toman <ivo.toman@ivorius.com>
2015-03-16 17:03:48 +0100

    fix handleDelete for sport

--- app/presenters/SportPresenter.php
+++ app/presenters/SportPresenter.php
@@ -46,6 +46,21 @@ class SportPresenter extends BasePresenter {
 	}
 	
 	
+	public function handleDelete($sport) {
+		$deleteEntity = $this->sports->findOneBy(["id" => $sport, "school" => $this->school, "is_global" => FALSE]);
+		if($deleteEntity) {
+			try {
+			 $this->sports->delete($deleteEntity);
+			 $this->flashMessage("Sport " . $deleteEntity->name . " byl vymazán.", "info");
+			} catch(\Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException $e) {
+				$this->flashMessage("Na tento sport jsou navázány další záznamy. Nelze jej odstranit.");
+			}
+		} else {
+			$this->flashMessage("Tento sport již nemáte právo editovat", "error");
+		}
+		$this->redirect('this');
+	}
+	
 	
 
 	protected function createComponentEditSport() {
